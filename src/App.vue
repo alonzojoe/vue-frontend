283e6d3c9f096453a68fd22eb072da0568451f92
Joenell Alonzo <alonzojoenell2k15@gmail.com>
2025-03-07 14:18:28 +0800

    feat: setup boiler plate vue-router,pinia and tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,12 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="text-indigo-500">
+    <h2>Vue 3 (Composition API), Pinia (State Management), Vue Router (Routing), Tailwind (Styling)</h2>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
